213097c230c51aa0ddc029d7ea5b101647e336fb
DeCinB <50006398+DeCinB@users.noreply.github.com>
2019-12-16 14:06:10 +0800

    Add files via upload

--- OrderManage1.0.1/business/urls.py
+++ OrderManage1.0.1/business/urls.py
@@ -5,7 +5,7 @@ from business.views import login,manage,lookover,home
 urlpatterns=[
     path('', login.login), #登录
     path('reg/',login.reg,name='check'), #登录
-    path('home',home.home)
+    path('home',home.home),
     path('managestaff',manage.staff), #管理员工
     path('managemenu',manage.menu),#管理菜单
     path('income',lookover.income),#查看收入情况
