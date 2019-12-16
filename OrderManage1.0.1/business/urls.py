f29f81be4db944a2e0f006c5f53945c0d2b753b6
DeCinB <50006398+DeCinB@users.noreply.github.com>
2019-12-16 13:49:00 +0800

    更新路由

--- OrderManage1.0.1/business/urls.py
+++ OrderManage1.0.1/business/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path,include
-from business.views import login
+from business.views import login,manage,lookover,home
 
 
 urlpatterns=[
-    path('', login.login), #?name来干嘛'''name='login'''
-    path('reg/',login.reg,name='check'),
-
+    path('', login.login), #登录
+    path('reg/',login.reg,name='check'), #登录
+    path('home',home.home)
+    path('managestaff',manage.staff), #管理员工
+    path('managemenu',manage.menu),#管理菜单
+    path('income',lookover.income),#查看收入情况
+    path('analyze',lookover.analyze),#查看菜品销售分析
 ]
 
