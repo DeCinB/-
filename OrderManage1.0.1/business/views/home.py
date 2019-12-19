07c43ee9702da7708c4709dd7a290190545080b7
yamagata1 <35889443+yamagata1@users.noreply.github.com>
2019-12-19 11:34:10 +0800

    完善了home界面的功能

--- OrderManage1.0.1/business/views/home.py
+++ OrderManage1.0.1/business/views/home.py
@@ -1,11 +1,36 @@
 #商家首页
 from django.shortcuts import render
 from django.http import HttpResponse
-from business.models import Cuisine,CuisineKind,Desk,Order,OrderDetail
-from django.http import JsonResponse
+from business.models import Order,OrderDetail
 import json
-import time
 
 def home(request):
 	#获得主页
-	return render(request,'business/home.html')
+	if request.method == 'GET':
+		all_order = Order.objects.all()
+		unpaid_order =[]
+		dish ={}
+		for order in all_order:
+			if not order.pay:
+				unpaid_order.append(order)
+				dish[order.order_id] = order.orderdetail_set.all()
+
+		return render(request,'business/home.html',
+					{
+						'unpaid_list': unpaid_order,
+						'dish_dict' : dish,
+					})
+	if request.method=='POST':
+		if request.POST:
+			if request.POST['order_id']:
+				order_id = request.POST['order_id']
+				desk = Order.objects.get(order_id=order_id)
+				desk.pay = True
+				desk.save()
+
+		return HttpResponse(json.dumps({
+			"status": 'success',
+		}
+		))
+
+
